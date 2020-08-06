a45398d3b57ca533268090bf32a961db0f1dc78a
Ryan Molden <rmolden@microsoft.com>
2020-08-05 20:48:43 -0700

    Fix (hack-fix) to support SYMSRV*<rando dll>*<cache dir>*<remote> symbol server syntax

--- src/Microsoft.Diagnostics.Runtime/src/Implementation/SymbolServerLocator.cs
+++ src/Microsoft.Diagnostics.Runtime/src/Implementation/SymbolServerLocator.cs
@@ -5,8 +5,11 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Diagnostics.Runtime.Utilities;
 
@@ -43,12 +46,16 @@ namespace Microsoft.Diagnostics.Runtime.Implementation
                 string location = split[split.Length - 1];
 
                 string? cache = null;
+                string? symbolServerDll = null;
+
                 if (split.Length > 2)
                 {
                     cache = split[split.Length - 2];
 
+                    // TODO: Is this right? I don't put "cache" in my NT_SYMBOL_PATH, but I do still have a symbol cache
                     if (cache.Equals("cache", StringComparison.OrdinalIgnoreCase))
                     {
+                        // TODO: How can this be null of cache.Equals was "cache"
                         if (cache == null)
                         {
                             // case: cache*c:\location
@@ -62,9 +69,23 @@ namespace Microsoft.Diagnostics.Runtime.Implementation
 
                     if (cache.Equals("srv") || cache.Equals("svr"))
                         cache = null;
+
+                    if(split.Length > 3)
+                    {
+                        // This handles an old format (used in some places) that is SYMSRV*<some dll name>*cache path*remote store
+                        //
+                        // In this case we must load <some dll name> and invoke the SymbolServer API contract that symsrv.dll lays out (and is undocumented). This allows
+                        // people to redirect symbol server access through an arbitrary dll that implements a C-style interface. It sounds bizaare, but some systems inside
+                        // Microsoft use this technique.
+                        string temp = split[split.Length - 3];
+                        if (!temp.Equals("srv") && !temp.Equals("svr") && temp.EndsWith(".dll"))
+                        {
+                            symbolServerDll = temp;
+                        }
+                    }
                 }
 
-                paths.Add(new SymbolPathEntry(cache, location));
+                paths.Add(new SymbolPathEntry(cache, location, symbolServerDll));
             }
 
             _paths = paths.ToArray();
@@ -136,14 +157,14 @@ namespace Microsoft.Diagnostics.Runtime.Implementation
                 // Unfortunately this has to be called under a lock.  We need to make sure that multiple threads
                 // do not race to download/copy the same file to a local path.  We will simply make sure that
                 // FindBinaryFromServerAsync will 'await' before doing too much work.
-                result = FindBinaryFromServerAsync(indexPath);
+                result = FindBinaryFromServerAsync(indexPath, buildTimeStamp, imageSize);
                 _queries.Add(indexPath, result);
 
                 return result;
             }
         }
 
-        private async Task<string?> FindBinaryFromServerAsync(string indexPath)
+        private async Task<string?> FindBinaryFromServerAsync(string indexPath, int buildTimeStamp, int imageSize)
         {
             // We assume that if we got the file from the symbol server that it matches the
 
@@ -151,7 +172,7 @@ namespace Microsoft.Diagnostics.Runtime.Implementation
             {
                 if (entry.IsHttp)
                 {
-                    string? result = await FindBinaryFromServerAsync(entry.Location, entry.Cache ?? _cache, indexPath).ConfigureAwait(false);
+                    string? result = await FindBinaryFromServerAsync(entry.Location, entry.Cache ?? _cache, indexPath, entry.SymServerDll, buildTimeStamp, imageSize).ConfigureAwait(false);
                     if (result != null)
                         return result;
                 }
@@ -160,10 +181,15 @@ namespace Microsoft.Diagnostics.Runtime.Implementation
             return null;
         }
 
-        private static async Task<string?> FindBinaryFromServerAsync(string server, string cache, string indexPath)
+        private static async Task<string?> FindBinaryFromServerAsync(string server, string cache, string indexPath, string? symSrvDll, int buildTimeStamp, int imageSize)
         {
             // There are three ways symbol files can be indexed.  Start looking for each one.
             string fullDestPath = Path.Combine(cache, indexPath);
+
+            if (symSrvDll != null)
+            {
+                return await GetPhysicalFileFromServerUsingSymSrvDllAsync(server, GetPathFromIndexPath(indexPath), symSrvDll, cache, buildTimeStamp, imageSize).ConfigureAwait(continueOnCapturedContext: false);
+            }
 
             // First, check for the compressed location.  This is the one we really want to download.
             string compressedFilePath = indexPath.Substring(0, indexPath.Length - 1) + "_";
@@ -238,6 +264,64 @@ namespace Microsoft.Diagnostics.Runtime.Implementation
 
             Trace($"No file matching '{Path.GetFileName(indexPath)}' found on server '{server}'.");
             return null;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr LoadLibrary(string dllToLoad);
+
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetProcAddress(IntPtr module, string procName);
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private delegate bool SymbolServer([MarshalAs(UnmanagedType.LPWStr)] string? serverAndCachePath,
+                                           [MarshalAs(UnmanagedType.LPWStr)] string fileName,
+                                           IntPtr id,
+                                           uint two,
+                                           uint three,
+                                           [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder filePath);
+
+        private static Dictionary<string, IntPtr> LoadedSymServerDlls = new Dictionary<string, IntPtr>();
+
+        private static Task<string?> GetPhysicalFileFromServerUsingSymSrvDllAsync(string server, string fileName, string symServerDll, string cachePath, int buildTimeStamp, int imageSize)
+        {
+            return Task.Run(() =>
+                {
+                    IntPtr moduleHandle;
+                    lock (LoadedSymServerDlls)
+                    {
+                        if (!LoadedSymServerDlls.TryGetValue(symServerDll, out moduleHandle))
+                        {
+                            LoadedSymServerDlls[symServerDll] = moduleHandle = LoadLibrary(symServerDll);
+
+                            if (moduleHandle == IntPtr.Zero)
+                            {
+                                Trace($"LoadLibrary on '{symServerDll}' returned: 0x'{Marshal.GetLastWin32Error()}.");
+                                return (string?)null;
+                            }
+                        }
+                    }
+
+                    // We failed (on a previous call) to load the dll, we don't perpetually try, now we will just immediately fail
+                    if(moduleHandle == IntPtr.Zero)
+                        return (string?)null;
+
+                    IntPtr symbolServerWAddress = GetProcAddress(moduleHandle, "SymbolServerW");
+                    if (symbolServerWAddress == IntPtr.Zero)
+                    {
+                        Trace($"GetProcAddress on '{symServerDll}' for 'SymbolServerW' returned: 0x'{Marshal.GetLastWin32Error()}.");
+                        return (string?)null;
+                    }
+
+                    StringBuilder outPath = new StringBuilder(260);
+                    SymbolServer symbolServerWCallback = (SymbolServer)Marshal.GetDelegateForFunctionPointer(symbolServerWAddress, typeof(SymbolServer));
+                    if (!symbolServerWCallback($"{cachePath}*{server}", fileName, (IntPtr)(int)buildTimeStamp, (uint)imageSize, three: 0, outPath))
+                    {
+                        Trace($"SymbolServerW in '{symServerDll}' for '{fileName}' returned: 0x'{Marshal.GetLastWin32Error()}.");
+                        return (string?)null;
+                    }
+
+                    return outPath.ToString();
+                });
         }
 
         private static async Task<string?> GetPhysicalFileFromServerAsync(string serverPath, string fileIndexPath, string fullDestPath, bool returnContents = false)
@@ -326,6 +410,12 @@ namespace Microsoft.Diagnostics.Runtime.Implementation
         }
 
 #pragma warning disable CA1308 // Normalize strings to uppercase, symbol server expects lowercase
+        private static string GetPathFromIndexPath(string indexPath)
+        {
+            return indexPath.Substring(0, indexPath.IndexOf("\\"));
+        }
+
+        // NOTE: If this were to change GetPathFromIndexPath also needs to be changed to understand how to extract the orgiinal filename out of the formed index path
         private static string GetIndexPath(string fileName, int buildTimeStamp, int imageSize)
         {
             fileName = Path.GetFileName(fileName).ToLowerInvariant();
@@ -339,8 +429,9 @@ namespace Microsoft.Diagnostics.Runtime.Implementation
             public string Location { get; }
             public bool IsHttp => Location.StartsWith("http:") || Location.StartsWith("https:");
             public bool IsLocal => !IsHttp && !new Uri(Location).IsUnc;
+            public string? SymServerDll { get; }
 
-            public SymbolPathEntry(string? cache, string location)
+            public SymbolPathEntry(string? cache, string location, string? symServerDll)
             {
                 if (CreateCache(cache))
                     Cache = cache;
@@ -348,6 +439,7 @@ namespace Microsoft.Diagnostics.Runtime.Implementation
                     Cache = null;
 
                 Location = location;
+                SymServerDll = symServerDll;
             }
 
             private static bool CreateCache(string? cache)
